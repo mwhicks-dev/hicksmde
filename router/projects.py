68e4c7db0fb6f6cc4d614f31820d567dbd8fb265
Mason <mason@hicksm.dev>
2024-08-23 13:56:44 -0400

    Implemented router for projects feature #1

---
+++ router/projects.py
@@ -0,0 +1,12 @@
+import requests
+
+from fastapi import APIRouter, HTTPException
+
+ProjectsRouter = APIRouter()
+
+@APIRouter.get("/projects")
+async def read_projects():
+    response = requests.get("https://hicksm.dev/static/v1/wip/wip.html")
+    if response.ok:
+        return response.content
+    raise HTTPException(404)
