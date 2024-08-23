38820add4b29d33694d1b35f0b90f1029b9326e8
Mason <mason@hicksm.dev>
2024-08-23 15:52:29 -0400

    Set up static files

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from router import routers
 
 app = FastAPI()
 for router in routers:
     app.include_router(router)
+
+app.mount("/static", StaticFiles(directory="static"))
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=8000, reload=True)
