14439680f1a454f0c4eef33228c48bc23c28875a
Mason <mason@hicksm.dev>
2024-08-23 13:55:20 -0400

    Implemented router for blog feature #1

---
+++ router/blog.py
@@ -0,0 +1,12 @@
+import requests
+
+from fastapi import APIRouter, HTTPException
+
+BlogRouter = APIRouter()
+
+@APIRouter.get("/blog")
+async def read_blog():
+    response = requests.get("https://hicksm.dev/static/v1/wip/wip.html")
+    if response.ok:
+        return response.content
+    raise HTTPException(404)
