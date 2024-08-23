c8e5c550714b2c7352e55f933baad3ddc50ed254
Mason <mason@hicksm.dev>
2024-08-23 14:01:50 -0400

    Created main function #1

---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+from router import routers
+
+app = FastAPI()
+for router in routers:
+    app.include_router(router)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="hicksm.dev", port=8000, reload=True)
+    
