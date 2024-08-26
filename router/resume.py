1c9eab2b2f84423a5c2d66fcd813d3e9fe62d3f7
Mason <mason@hicksm.dev>
2024-08-26 07:01:27 -0400

    Added resume page to mapping as submodule

--- router/resume.py
+++ router/resume.py
@@ -7,6 +7,6 @@ ResumeRouter = APIRouter()
 
 @ResumeRouter.get("/resume")
 async def serve_resume():
-    with open("static/hicksmde-wip/v1/wip.html", "r") as fp:
+    with open("static/hicksmde-resume/v1/resume.html", "r") as fp:
         content = ''.join(fp.readlines())
     return HTMLResponse(content, 200)
