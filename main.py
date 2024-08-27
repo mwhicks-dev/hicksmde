2339c7b50b4228e792bbb83047e2f6fecc8b37b9
Mason <mason@hicksm.dev>
2024-08-27 09:30:25 -0400

    Reintroduced main func #2

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ from router import routers
 app = FastAPI()
 for router in routers:
     app.include_router(router)
+
+app.mount("/static", StaticFiles(directory="static"))
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", port=80, reload=True)
