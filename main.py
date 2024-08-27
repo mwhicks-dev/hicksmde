c5b5d4983e0da655df074498e20b1bca4b35333d
Mason <mason@hicksm.dev>
2024-08-27 09:21:31 -0400

    Removed runtime config from main #2

--- main.py
+++ main.py
@@ -8,9 +8,3 @@ from router import routers
 app = FastAPI()
 for router in routers:
     app.include_router(router)
-
-app.mount("/static", StaticFiles(directory="static"))
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", port=8000, reload=True)
-    
