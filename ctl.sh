93a96728c817bf05307ceb0ef85f0c843051d4b4
Mason Hicks <63812007+mwhicks-dev@users.noreply.github.com>
2024-08-30 12:14:26 -0400

    Deploy script now builds before stopping server

--- ctl.sh
+++ ctl.sh
@@ -1,4 +1,4 @@
 # Use this script to redeploy the containerized devsite to a host.
-docker stop hicksmde
 docker build --no-cache -t hicksmde --build-arg TARGET=dev .
+docker stop hicksmde
 docker run --name hicksmde --rm -d -p 80:80 hicksmde
