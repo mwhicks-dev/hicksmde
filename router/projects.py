1023a5cac9c163e5e034bdff86e16e895c70c9ed
Mason <mason@hicksm.dev>
2024-08-30 13:04:31 -0400

    Implemented projects page

--- router/projects.py
+++ router/projects.py
@@ -7,6 +7,12 @@ ProjectsRouter = APIRouter()
 
 @ProjectsRouter.get("/projects")
 async def serve_projects():
-    with open("static/hicksmde-wip/v1/wip.html", "r") as fp:
+    with open("static/hicksmde-projects/v1/projects.html", "r") as fp:
+        content = ''.join(fp.readlines())
+    return HTMLResponse(content, 200)
+
+@ProjectsRouter.get("/project/{project_id}")
+async def serve_project(project_id: str):
+    with open("static/hicksmde-projects/v1/project.html", "r") as fp:
         content = ''.join(fp.readlines())
     return HTMLResponse(content, 200)
