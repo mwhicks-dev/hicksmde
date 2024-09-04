636d9aa2a9ed627d35eb3dbc9908d2039ea5facc
Mason <mason@hicksm.dev>
2024-09-04 10:37:27 -0400

    Added configuration for PyAcct and AuthMail dependencies

--- router/uri.py
+++ router/uri.py
@@ -1,12 +1,24 @@
-import os
+import json
 from fastapi import APIRouter, HTTPException
 
 UriRouter = APIRouter()
 
 @UriRouter.get("/uri/pyacct")
 async def serve_pyacct_uri():
+    with open("config.json", "r") as fp:
+        config = json.load(fp)
     try:
-        return os.environ['URI_PYACCT']
+        return config['pyacct_uri']
+    except KeyError:
+        return HTTPException(status_code=404, 
+                             detail="No URI set for PyAcct")
+    
+@UriRouter.get("/uri/authmail")
+async def serve_authmail_uri():
+    with open("config.json", "r") as fp:
+        config = json.load(fp)
+    try:
+        return config['authmail_uri']
     except KeyError:
         return HTTPException(status_code=404, 
                              detail="No URI set for PyAcct")
