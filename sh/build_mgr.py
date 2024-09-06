072ebb124a4faa15ac44d6e6440ec8eb23830c12
Mason <mason@hicksm.dev>
2024-09-06 09:59:36 -0400

    Updated build_mgr to more robust version

--- sh/build_mgr.py
+++ sh/build_mgr.py
@@ -18,14 +18,18 @@ def main():
 
     # setup time stuff
     future = datetime.now()
-    future.hour = 23
-    future.minute = 59
-    future.second = 59
+    future = future.replace(hour=23, minute=59, second=59)
 
     while future < datetime.now():
-        future.day += 1
+        curr_day = future.day
+        future = future.replace(day=curr_day + 1)
     
     while True:
         seconds_until_future = (future - datetime.now()).total_seconds
         time.sleep(seconds_until_future)
         os.system(script)
+        curr_day = future.day
+        future = future.replace(day=curr_day + 1)
+
+if __name__ == '__main__':
+    main()
