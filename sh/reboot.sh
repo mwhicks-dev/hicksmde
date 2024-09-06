9c2c557a2f2269d4a875e7e1b186ac26efe26f0f
Mason <mason@hicksm.dev>
2024-09-06 09:35:58 -0400

    Corrected reboot dir

--- sh/reboot.sh
+++ sh/reboot.sh
@@ -1,10 +1,16 @@
-${HOME}/git/hicksmde-mapping/network.sh || true
-sleep 1
-${HOME}/git/hicksmde-mapping/build.sh || true
-sleep 1
-${HOME}/git/hicksmde-mapping/stop.sh || true
-sleep 1
-${HOME}/git/hicksmde-mapping/rm.sh || true
-sleep 1
-${HOME}/git/hicksmde-mapping/run.sh || true
+cd ${HOME}/git/hicksmde-mapping/sh
 
+git restore ./*
+git pull
+
+./network.sh || true
+sleep 1
+./build.sh || true
+sleep 1
+./stop.sh || true
+sleep 1
+./rm.sh || true
+sleep 1
+./run.sh || true
+
+cd -
